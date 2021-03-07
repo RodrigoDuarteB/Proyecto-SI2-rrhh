1c8df4cd0a92d6e63fee965f076fa3df127fd16b
faisal00paz <faisal0paz@gmail.com>
2021-03-07 05:03:05 -0400

    web?

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\PermissionController;
 use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\DepartmentController;
+use Spatie\Permission\Models\Permission;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,9 +19,12 @@ use App\Http\Controllers\DepartmentController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::view('index', 'index')->name('index');
 
 Route::middleware(['auth'])->group(function (){
     Route::view('/', 'home')->name('home');
+    Route::view('/error', 'error')->name('error');
+    Route::view('/error405', 'error405')->name('error405');
     Route::resource('users', UserController::class)->names('users');
     Route::resource('departments', DepartmentController::class)->names('departments');
     Route::resource('orders', OrderController::class)->names('orders');
@@ -29,14 +34,27 @@ Route::middleware(['auth'])->group(function (){
 });
 
 
-//RUTA PARA CREAR USUARIOS DE PRUEBA 
+//RUTA PARA CREAR USUARIOS DE PRUEBA
 Route::get('/test', function (){
     $user = new \App\Models\User();
+<<<<<<< HEAD
     $user->name = 'Romina0Cortez';
     $user->email = 'rominacortez@outlook.com';
     $user->password = bcrypt('laravel1234');
+=======
+    $user->name = 'Prueba';
+    $user->email = 'prueba@gmail.com';
+    $user->password = bcrypt('prueba');
+    $user->type = \App\Models\User::$ADMINISTRATOR;
+>>>>>>> e4ce6d1018581eccf708707966252573e2349053
     $user->save();
+    $user->syncPermissions(['Gestionar Personal']);
     return $user;
+});
+
+Route::get('/test2', function (){
+    $permission = Permission::where('name', '=', 'Crear Empleados')->get();
+    return dd($permission);
 });
 
 require __DIR__.'/auth.php';

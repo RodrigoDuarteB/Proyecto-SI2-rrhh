7a916a4ab569a3dedcfcb97476d29b49e4bdb007
Alfr0rax <Alfr0rax@users.noreply.github.com>
2021-03-12 00:41:51 -0400

    Views( Cargos, Contratos)

--- resources/views/contracts/edit.blade.php
+++ resources/views/contracts/edit.blade.php
@@ -4,27 +4,27 @@
     <h3 class="text-dark mb-1" style="padding-bottom: 17px;"><strong>Contratos</strong><br></h3>
     <div class="card shadow mb-3">
         <div class="card-header py-3">
-            <p class="text-primary m-0 font-weight-bold">Crear Contrato</p>
+            <p class="text-primary m-0 font-weight-bold">Editar Contrato</p>
         </div>
         <div class="card-body">
             <div class="form-row">
                 <div class="col">
                     <div class="form-group" style="margin-bottom: 30px;"><label><strong>Nombre
                                 :</strong><br></label><input type="text" name="name" value="name"
-                            style="margin-left: 25px;width: 290px;"></div>
+                            style="margin-left: 25px;width: 290px;" disabled=""></div>
                     <div class="form-group" style="margin-top: 33px;margin-bottom: 46px;"><label><strong>Descripción
                                 :</strong><br></label><textarea style="margin-bottom: -35px;margin-left: 10px;"
                             name="description"></textarea></div>
                 </div>
                 <div class="col">
                     <div class="form-group"><label><strong>ID Empleado:</strong><br></label><input type="text"
-                            name="id_employee" value="id" style="margin-left: 25px;"></div>
+                            name="id_employee" value="id" style="margin-left: 25px;" disabled=""></div>
                 </div>
             </div>
             <div class="form-row">
                 <div class="col">
                     <div class="form-group"><label><strong>Fecha Inicio :</strong><br></label><input type="date"
-                            name="date_start" style="margin-left: 20px;"></div>
+                            name="date_start" style="margin-left: 20px;" disabled=""></div>
                     <div class="form-group"><label for="last_name"><strong>Cargo :</strong><br></label><select
                             class="form-control" id="exampleSelect-2" style="min-width: 0px|;width: 50%;">
                             <option value="">Tipo1</option>
@@ -53,17 +53,16 @@
                             <option value="">Tipo1</option>
                             <option value="">Tipo2</option>
                             <option value="">Tipo3</option>
-                        </select>
-                    </div>
+                        </select></div>
                 </div>
             </div>
             <div class="row justify-content-center h-100" style="margin-top: 13px;padding-top: 40px;">
                 <div class="col-sm-8 align-self-center text-center"><button class="btn btn-success" type="submit"
                         style="margin-right: 77px;width: 95px;">Crear</button><a class="btn btn-danger" role="button"
-                        href="#">Cancelar</a>
-                </div>
+                        href="#">Cancelar</a></div>
             </div>
         </div>
     </div>
 </div>
+
 @endsection
